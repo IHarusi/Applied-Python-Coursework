e17b407033e519d26bc0e6f91dca109bd9c43bb4
Fabrizio Boza Ventocilla <fbozven2005@gmail.com>
2026-01-13 18:45:23 -0500

    Enhance operadores.py with comments and print updates
    Added comments to clarify operations and updated print statements.

--- operadores.py
+++ operadores.py
@@ -1,7 +1,14 @@
 z = 20 
 v = 30
+# + : suma
+# - : resta
+# ** : potencia
+# * : multiplicación
 
-print(z/v)
+print(z/v) #refiere a dar la division sin resto
 
 res = z // v
-print(res)
+print(res) #refiere a dar el resto de la división
+
+
+
